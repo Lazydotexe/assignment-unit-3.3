63ac6516ce56427f43a04b940bee5627eb3efe6b
LazyDotExe <papaporo@proton.me>
2023-06-30 15:31:58 -0500

    I commented out the logs i made. and i set supplychanges.pop() to a variable. I also added a background to the css file. I never looked inside the css file to know that was required.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,33 +4,36 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 let partsNeeded = 40;
-console.log('1. Number of partsNeeded:', partsNeeded);
+console.log(partsNeeded);
 
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 const supplyChanges = [3, 5, -6, 0, 7, 11]; 
-console.log('2. Array of supplyChanges:', supplyChanges);
+// console.log('2. Array of supplyChanges:', supplyChanges);
+
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:', supplyChanges[1]);
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:', supplyChanges.pop());
+let removedItem = supplyChanges.pop(); //sets a new variable = the element removed by .pop()
+
+console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 supplyChanges.push(25);
-console.log('5. Adding 25 to supplyChanges.')
+// console.log('5. Adding 25 to supplyChanges.')
 //console.log(supplyChanges);
-console.log(supplyChanges);
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
-console.log('6. Showing supplyChanges...'); 
+// console.log('6. Showing supplyChanges...'); 
 
 for (let i = 0; i < supplyChanges.length; i += 1)  {
     
@@ -51,7 +54,7 @@ for (let i = 0; i < supplyChanges.length; i += 1)  {
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
-console.log('7. Showing supplyChanges with "for of" loop');
+// console.log('7. Showing supplyChanges with "for of" loop');
 for (const ledger of supplyChanges) {
     if (ledger > 0) {
       console.log(`Added ${ledger} parts.`);
@@ -64,7 +67,7 @@ for (const ledger of supplyChanges) {
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('8. Total supplies available is:');
+// console.log('8. Total supplies available is:');
 
 let totalSuppy = 0;
 
@@ -72,7 +75,7 @@ for (let i = 0; i < 6; i++) {
     totalSuppy += supplyChanges[i];
 }
 
-console.log(totalSuppy);
+// console.log(totalSuppy);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -80,7 +83,7 @@ console.log(totalSuppy);
 //    Use a `while` loop to keep adding parts to boxes until
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
-console.log('9. Filling boxes with a "while" loop');
+// console.log('9. Filling boxes with a "while" loop');
 
 let array = [];
 let stash = 572;
@@ -92,4 +95,4 @@ while (stash >= 7) {
     
 }
 
-console.log(`There were a total of ${array.length} boxes filled with ${stash} remaining`);
+// console.log(`There were a total of ${array.length} boxes filled with ${stash} remaining`);
